2a996a4f045ecd11a0e79f2beb963d47f59847c5
Anton Belyaev <anton.belyaev@bostongene.com>
2018-08-31 18:55:29 +0300

    added liquibase with spring_session table creation changeset. rolled back to Boot 1.5.10

--- src/main/java/ru/belyaev/WebSecurityConfiguration.java
+++ src/main/java/ru/belyaev/WebSecurityConfiguration.java
@@ -5,9 +5,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author anton.belyaev@bostongene.com
@@ -16,32 +13,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("admin").password("qwerty").roles("ADMIN")
-//                .and()
-//                .withUser("superadmin").password("qazxsw").roles("ADMIN", "SUPERADMIN");
-//    }
-
-    /**
-     * Spring Boot 2.0 requires PasswordEncoder instance. but it can also be replaced with the following
-     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser(User
-                        .withUsername("admin")
-                        .password("qwerty")
-                        .roles("ADMIN")
-                        .passwordEncoder(encoder::encode))
-                .withUser(User
-                        .withUsername("superadmin")
-                        .password("qazxsw")
-                        .roles("ADMIN", "SUPERADMIN")
-                        .passwordEncoder(encoder::encode));
+                .withUser("admin").password("qwerty").roles("ADMIN")
+                .and()
+                .withUser("superadmin").password("qazxsw").roles("ADMIN", "SUPERADMIN");
     }
+
+//    /**
+//     * Spring Boot 2.0 requires PasswordEncoder instance. but it can also be replaced with the following
+//     */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(User
+//                        .withUsername("admin")
+//                        .password("qwerty")
+//                        .roles("ADMIN")
+//                        .passwordEncoder(encoder::encode))
+//                .withUser(User
+//                        .withUsername("superadmin")
+//                        .password("qazxsw")
+//                        .roles("ADMIN", "SUPERADMIN")
+//                        .passwordEncoder(encoder::encode));
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
