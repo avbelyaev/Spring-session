c357d4f334ae93b305ecf70648e5505f421c5412
Anton Belyaev <anton.belyaev@bostongene.com>
2018-08-31 16:15:53 +0300

    switched to redis session-store

--- src/main/java/ru/belyaev/SessionConfiguration.java
+++ src/main/java/ru/belyaev/SessionConfiguration.java
@@ -1,7 +1,9 @@
 package ru.belyaev;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 /**
@@ -9,22 +11,19 @@ import org.springframework.session.web.context.AbstractHttpSessionApplicationIni
  */
 public class SessionConfiguration {
 
-//    @Configuration
-//    @EnableRedisHttpSession
-//    public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
-//
-//        @Bean
-//        public JedisConnectionFactory connectionFactory() {
-//            return new JedisConnectionFactory();
-//        }
-//    }
-
-    /**
-     * spring_session table should be created manually https://stackoverflow.com/a/37741000/4504720
-     */
     @Configuration
-    @EnableJdbcHttpSession
-    public static class JdbcSessionConfig extends AbstractHttpSessionApplicationInitializer {
+    @EnableRedisHttpSession
+    public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
 
+        @Bean
+        public JedisConnectionFactory connectionFactory() {
+            return new JedisConnectionFactory();
+        }
     }
+
+//    @Configuration
+//    @EnableJdbcHttpSession
+//    public static class JdbcSessionConfig extends AbstractHttpSessionApplicationInitializer {
+//
+//    }
 }
